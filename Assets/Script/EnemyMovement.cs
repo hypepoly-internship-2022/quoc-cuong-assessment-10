48b5581ae30b219690221bcb54525d6c502ba35d
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-17 16:30:56 +0700

    +add script enemy

--- Assets/Script/EnemyMovement.cs
+++ Assets/Script/EnemyMovement.cs
@@ -4,15 +4,71 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] private float speedUNeed;
+    [SerializeField] private Material[] material;
+
+    private Rigidbody rigidBody;
+    private bool isCollision;
+    private float timeIdle;
+    private Renderer rend;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        speedUNeed = 5f;
+        timeIdle = 2f;
+        rigidBody = this.GetComponent<Rigidbody>();
+        rend = this.GetComponent<Renderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collisionInfo)
     {
-        
+        if(collisionInfo.collider.tag == "Wall")
+        {   
+            isCollision = true;
+            speedUNeed = -speedUNeed;
+        } 
+        else 
+        {
+            EndMove();
+        }
     }
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        if(isCollision == true)
+        {
+            timeEnemyIdle();
+        }
+        else
+        {
+            moveEnemy();
+        }
+    }
+
+    private void timeEnemyIdle(){
+        if(timeIdle > 0)
+        {
+            timeIdle -= Time.deltaTime;
+            rend.sharedMaterial = material[0];
+        }
+        else 
+        {
+            isCollision = false;
+            rend.sharedMaterial = material[1];
+            timeIdle = 2f;
+        }
+    }
+
+    private void EndMove()
+    {
+        this.GetComponent<EnemyMovement>().enabled = false;
+    }
+
+    private void moveEnemy() 
+    {
+        rigidBody.AddForce(0, 0, speedUNeed);
+    }
+
 }
